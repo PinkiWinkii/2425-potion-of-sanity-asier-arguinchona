c5c0797967a9cf28100a5d3663ae01e4c3492b79
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-10-08 13:08:24 +0200

    [MAIN] Add isPotionOfSanity function to cauldron Class

--- cauldron.mjs
+++ cauldron.mjs
@@ -29,4 +29,10 @@ export default  class Cauldron {
 
         
     }
+
+    isPotionOfSanity(i1, i2)
+    {
+        return i1.name === "Nightshade" && i2.name === "Ectoplasm" ||
+                i2.name === "Nightshade" && i1.name === "Ectoplasm" ? true : false;
+    }
 }
