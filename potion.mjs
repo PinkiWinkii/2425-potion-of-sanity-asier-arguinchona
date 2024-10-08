b1cc5812976594481d74e6470548baa79920a0d1
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-10-08 13:05:38 +0200

    [MAIN] Add PotionOfSanity and FailedPotion classes that inherit from Potion

--- potion.mjs
+++ potion.mjs
@@ -5,4 +5,31 @@ export default class Potion {
         this.value = value;
         this.time = time;
     }
+
+    static with(effect, weight, value) {
+        const type = effect.type === 'beneficial' ? "Potion" : "Poison";
+        const potion_name = `${type} of ${effect.name}`;
+        const time = 10;
+        return new Potion(potion_name, value, weight, time);
+    }
+
+    static failed() {
+        return new FailedPotion();
+    }
+
+    static sanity() {
+        return new PotionOfSanity();
+    }
+}
+
+class PotionOfSanity extends Potion {
+    constructor() {
+        super("Potion of Sanity", 1000, 1, 50);
+    }
+}
+
+class FailedPotion extends Potion {
+    constructor() {
+        super("Failed potion", 0, 0, 0)
+    }
 }
